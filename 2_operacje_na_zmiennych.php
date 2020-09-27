c0d329ce52c638fe493a892fd5b35549b09ad359
Piotr Sierszula <szerszen0509@gmail.com>
2020-09-27 11:55:53 +0200

    git push

---
+++ 2_operacje_na_zmiennych.php
@@ -0,0 +1,24 @@
+<?php
+//operacje bitowe
+//and &, or |,not ~, xor ^,<<,>>
+$x = 0b1010;
+$x = $x <<2;
+$x = $x >>1;
+echo"$x<br>" ;
+//porównanie
+$x=1;
+$y=1;
+if ($x==$y) {
+    echo'$x jest równa $y'<br>;
+}
+else{
+    echo'$x jest równa $y'<br>;
+}
+if ($x===$y) {
+    echo'$x jest równa $y'<br>;
+}
+else{
+    echo'$x nie jest identyczna $y'<br>;
+}
+echo
+?>
