029f8eb652b63f84e07f300b52e7aaf86be0de63
Piotr Sierszula <szerszen0509@gmail.com>
2020-09-27 12:07:36 +0200

    gir push

--- 2_operacje_na_zmiennych.php
+++ 2_operacje_na_zmiennych.php
@@ -7,7 +7,7 @@ $x = $x >>1;
 echo"$x<br>" ;
 //porównanie
 $x=1;
-$y=1;
+$y=1.0;
 if ($x==$y) {
     echo'$x jest równa $y<br>';
 }
@@ -18,18 +18,32 @@ if ($x===$y) {
     echo'$x jest równa $y<br>';
 }
 else{
-    echo'$x nie jest identyczna $y'<br>;
+    echo'$x nie jest identyczna $y<br>';
 }
 echo gettype($x);//integer
-echo gettype($y)'<hr>';//double
+echo gettype($y);//double
 
 $x=-10;
 $y=1;
 
 echo $x<=>$y;
-echo <hr>;
+echo '<hr>';
 
 $x=2;
 $x=$x++;
 echo$x;
+$y=$x++;
+echo"$x<hr>";
+
+$x=1;
+$x=$x++;
+echo$x;
+$x=++$x;
+echo$x;
+$y=$x++;
+echo$x;
+echo$y;
+$y=++$x*2-1;
+echo$x;
+echo$y;
 ?>
