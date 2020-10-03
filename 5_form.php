0fed05341f00e7eb5cbd162eb927409f393f800c
Piotr Sierszula <szerszen0509@gmail.com>
2020-10-03 14:24:44 +0200

    push

--- 5_form.php
+++ 5_form.php
@@ -8,17 +8,33 @@
 <body>
     <form method="get">
     <input type="text" name="name">
-    <input type="submit" value="Wyślij dane">
+    <input type="submit" value="Wyślij dane" placeholder="Imię">
+    Regulamin:<input type="checkbox" name="info"><br>
+    Wybierz kolor:
+    <label><input type="radio" name="color2" checked> czarny</label>
+    <label><input type="radio" name="color2">biały</label>
+    <input type="color" name="color"><br>
     </form>
     <?php
-    if(isset($_GET['name'])){
+    if(!empty($_GET['name'])&& !empty($_GET['color']))
+    {
         $name = $_GET['name'];
+        $color = $_GET['color'];
         echo'Twoje imie: ',$name;
+        echo "<br>Kolor: $color";
     
     }
     else{
-        echo '<hr>Wypełnij dane!';
+        echo '<hr>Wypełnij wszystkie dane!';
     }
+    if (isset($_GET['info']))
+     {
+        echo'Zgody zatwierdzone';
+    }
+    else{
+        echo 'Zatwierzdz zgody';
+    }
+    //pola radio z wyborem jaki kolor(biały czy czarny)
     ?>
 </body>
 </html>
